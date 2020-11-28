a5b38b92fb8fbdad9fca9ff3da81c0c6a1379cc8
ninja-nick-s <ni773796@gmail.com>
2020-11-29 01:06:52 +0530

    added data in database model

--- app.js
+++ app.js
@@ -13,9 +13,81 @@ app.set('view engine', 'ejs');
 // app.use(bodyParser.urlencoded({
 //   extended: true
 // }));
-mongoose.connect("mongodb://localhost:27017/modelname", {useNewUrlParser : true,useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/healthDB", {useNewUrlParser : true,useUnifiedTopology: true});
 
 mongoose.set('useFindAndModify', false);
+
+const healthschema = {
+    title: String ,
+    location_lat1 : String ,
+    location_lon1 : String ,
+    location_lat2 : String ,
+    location_lon2 : String
+}
+
+const Health = mongoose.model("Health" , healthschema);
+
+app.post("/api-a" ,function(req ,res){
+    console.log(req.body);
+    const data = req.body;
+    res.json({
+        latitude : data.lat_a, 
+        longitude : data.lon_a
+    });
+    const lat = req.body.lat_a;
+    const lon = req.body.lon_a;
+    Health.findOneAndUpdate(
+        {title: "location"},
+        {$set : { location_lat2: lat }},
+        function(err){
+            if(!err){
+                console.log("successfully updated");
+            }
+            else{
+                console.log("unsuccessful");
+            }
+        }
+    );
+    Health.findOneAndUpdate(
+        {title: "location"},
+        {$set : { location_lon2: lon }},
+        function(err){
+            if(!err){
+                console.log("successfully updated");
+            }
+            else{
+                console.log("unsuccessful");
+            }
+        }
+    );
+
+});
+app.post("/api" ,function(req ,res){
+    console.log(req.body);
+    const data = req.body;
+    res.json({
+        latitude : data.lat, 
+        longitude : data.lon
+    });
+    const lat = req.body.lat;
+    const lon = req.body.lon;
+    const newhealth = new Health ({
+        title: "location",
+        location_lat1 : lat,
+        location_lon1 : lon,
+        location_lat2: "0",
+        location_lon2:"0"
+    });
+    newhealth.save();
+});
+
+app.get("/auth",function(req ,res){
+    res.render("auth");
+});
+
+app.post("/newloc",function(req,res){
+    res.redirect("/auth");
+});
 
 app.get("/newloc",function(req ,res){
     res.render("loc_aft");
